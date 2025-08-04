12871fffb6d0885076428e1ecfaca49304ec25e2
Luimer Luces <luimerluces@gmail.com>
2025-08-04 14:15:49 -0400

    45342t34

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World Yankees New York Derek Jeter 2!')
+  res.send('Hello World Yankees New York Derek Jeter 2 Posada!')
 })
 
 app.get('/saludo/:nombre', (req, res) => {
